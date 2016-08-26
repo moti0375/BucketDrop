cccb58eb9efe137f7cd10a9a82091d415152f021
Moti Bartov <moti0375@gmail.com>
2016-08-26 13:50:42 +0300

    Initial commit, First commit from Android Studio

---
+++ app/src/main/java/com/bartovapps/bucketdrop/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bartovapps.bucketdrop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

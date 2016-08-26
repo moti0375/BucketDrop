f4b890bce5135beeea57ca50c7fb620f6bfdfc1b
Moti Bartov <moti0375@gmail.com>
2016-08-26 15:43:33 +0300

    New commit after change

--- app/src/main/java/com/bartovapps/bucketdrop/MainActivity.java
+++ app/src/main/java/com/bartovapps/bucketdrop/MainActivity.java
@@ -2,12 +2,18 @@ package com.bartovapps.bucketdrop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(LOG_TAG, "onCreate was called");
+
     }
 }
---
+++ app/src/main/java/com/bartovapps/bucketdrop/Test.java
@@ -0,0 +1,8 @@
+package com.bartovapps.bucketdrop;
+
+/**
+ * Created by BartovMoti on 08/26/16.
+ */
+public class Test {
+    String someString;
+}

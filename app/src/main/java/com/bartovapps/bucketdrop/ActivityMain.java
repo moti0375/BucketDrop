d680c3f5ec998240a1cac4e7227d3d8055873e6c
Moti Bartov <moti0375@gmail.com>
2016-08-27 22:24:21 +0300

    Add background with Glide library

--- app/src/main/java/com/bartovapps/bucketdrop/ActivityMain.java
+++ app/src/main/java/com/bartovapps/bucketdrop/ActivityMain.java
@@ -1,13 +1,17 @@
 package com.bartovapps.bucketdrop;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
 
     private static final String LOG_TAG = ActivityMain.class.getSimpleName();
-
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +19,14 @@ public class ActivityMain extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i(LOG_TAG, "onCreate was called");
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        initBackgroundImage();
 
+    }
 
+    private void initBackgroundImage(){
+        ImageView ivBackground = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this).load(R.drawable.background).centerCrop().into(ivBackground);
     }
 }

bded6b2bc918b9879a45a2605caac31a257f0c36
Moti Bartov <moti0375@gmail.com>
2016-09-03 00:34:51 +0300

    Added recycler view footer section

--- app/src/main/java/com/bartovapps/bucketdrop/adapters/DropsAdapter.java
+++ app/src/main/java/com/bartovapps/bucketdrop/adapters/DropsAdapter.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.bartovapps.bucketdrop.R;
@@ -18,9 +19,12 @@ import io.realm.RealmResults;
 /**
  * Created by BartovMoti on 08/31/16.
  */
-public class DropsAdapter extends RecyclerView.Adapter<DropsAdapter.DropsViewHolder> {
+public class DropsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private final static String TAG = DropsAdapter.class.getSimpleName();
+    public static final int NORMAL_TYPE = 10;
+    public static final int FOOTER_TYPE = 20;
+
 
     LayoutInflater mInflater;
     RealmResults<Drop> mItems;
@@ -32,13 +36,13 @@ public class DropsAdapter extends RecyclerView.Adapter<DropsAdapter.DropsViewHol
 
     private ArrayList<String> initValues() {
         ArrayList<String> dummyItems = new ArrayList<>();
-        for(int i = 1; i < 101; i++){
+        for (int i = 1; i < 101; i++) {
             dummyItems.add("Item " + i);
         }
         return dummyItems;
     }
 
-    public void updateDrops(RealmResults<Drop> results){
+    public void updateDrops(RealmResults<Drop> results) {
         Log.i(TAG, "updateDrops");
 
         this.mItems = results;
@@ -46,26 +50,36 @@ public class DropsAdapter extends RecyclerView.Adapter<DropsAdapter.DropsViewHol
     }
 
     @Override
-    public DropsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        Log.i(TAG, "View type: " + viewType);
+        if (viewType == FOOTER_TYPE) {
+            view = mInflater.inflate(R.layout.recycler_view_footer, parent, false);
+            RecyclerView.ViewHolder viewHolder = new FooterHolder(view);
+            return viewHolder;
+        } else {
+            view = mInflater.inflate(R.layout.drop_row_item, parent, false);
+            RecyclerView.ViewHolder viewHolder = new DropsViewHolder(view);
+            return viewHolder;
+        }
 
-        View view = mInflater.inflate(R.layout.drop_row_item, parent, false);
-
-        DropsViewHolder viewHolder = new DropsViewHolder(view);
-        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(DropsViewHolder holder, int position) {
-        Drop drop = mItems.get(position);
-        holder.tvRowItemDrop.setText(drop.getGoal());
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if(holder instanceof DropsViewHolder){
+            DropsViewHolder dropsViewHolder = (DropsViewHolder) holder;
+            Drop drop = mItems.get(position);
+            dropsViewHolder.tvRowItemDrop.setText(drop.getGoal());
+        }
     }
 
     @Override
     public int getItemCount() {
-        return mItems.size();
+        return mItems.size() + 1;
     }
 
-    public static class DropsViewHolder extends RecyclerView.ViewHolder{
+    public static class DropsViewHolder extends RecyclerView.ViewHolder {
         TextView tvRowItemDrop;
         TextView tvWhen;
 
@@ -76,4 +90,25 @@ public class DropsAdapter extends RecyclerView.Adapter<DropsAdapter.DropsViewHol
         }
 
     }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mItems == null || position < mItems.size()) {
+            return NORMAL_TYPE;
+        } else {
+            return FOOTER_TYPE;
+        }
+    }
+
+    public static class FooterHolder extends RecyclerView.ViewHolder {
+        Button button;
+
+        public FooterHolder(View itemView) {
+            super(itemView);
+            button = (Button) itemView.findViewById(R.id.tb_footer_button);
+        }
+
+    }
+
+
 }
